e0396723df0ef2c6fab0634add75bc08b09a435a
Mateo Inchaurrandieta <mateo.inchaurrandieta.13@ucl.ac.uk>
2014-03-05 19:14:00 +0000

    Project restructuring
    Corrected package name, added icon, UCL banner, names and copyright.

---
+++ gen/com/comp1008/group26/FlaxmanGallery/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.comp1008.group26.FlaxmanGallery;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
